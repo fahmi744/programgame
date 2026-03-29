1ff599ea7a9d06309c2070e45e038c16c9953ef7
fahmi idris <fahmiidris7474@gmail.com>
2026-03-29 13:02:02 +0700

    first commit unity project

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenuPanel;
+    public GameObject settingsPanel;
+
+    public void OpenSettings()
+    {
+        mainMenuPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        settingsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game keluar (di editor gak kelihatan)");
+    }
+}
